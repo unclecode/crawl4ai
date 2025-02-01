20920fa17b3eee0479115432002fa172d30b4f76
UncleCode <unclecode@kidocode.com>
2025-02-01 14:28:28 +0800

    refactor(docker): clean up import statements in server.py

--- deploy/docker/server.py
+++ deploy/docker/server.py
@@ -1,4 +1,5 @@
-import os, sys
+import os
+import sys
 sys.path.append(os.path.dirname(os.path.realpath(__file__)))
 from fastapi import FastAPI, HTTPException
 from fastapi.responses import StreamingResponse
